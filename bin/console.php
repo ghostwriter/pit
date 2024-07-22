218484407d8e6197255e61e899059108226e1f53
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-22 01:00:13 -0500

    Update bin/console.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bin/console.php
+++ bin/console.php
@@ -7,21 +7,24 @@ namespace Ghostwriter\Wip\Console;
 
 use Ghostwriter\Wip\Foo;
 
-use function fwrite;
-use function sprintf;
+use const PHP_EOL;
 
-use const STDERR;
+use function dirname;
+use function is_file;
+use function sprintf;
 
 /** @var ?string $_composer_autoload_path */
 (static function (string $composerAutoloadPath): void {
-    /** @psalm-suppress UnresolvableInclude */
-    require $composerAutoloadPath ?: fwrite(
-        STDERR,
-        sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
-    ) && exit(1);
+
+    if (! is_file($composerAutoloadPath)) {
+        echo sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath), PHP_EOL;
+        exit(1);
+    }
+
+    require $composerAutoloadPath;
 
     /**
      * #BlackLivesMatter.
      */
     echo (new Foo())->test();
-})($_composer_autoload_path ?? null);
+})($_composer_autoload_path ??= dirname(__DIR__, 2) . '/vendor/autoload.php');
